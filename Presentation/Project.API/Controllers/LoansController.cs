19c164021797cdb473c6c66bb7d442cc0b869813
VahidDev <vrashidli1@gmail.com>
2023-01-28 18:50:28 +0400

    added simple api service for external system

---
+++ Presentation/Project.API/Controllers/LoansController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.Service.Services.Abstraction;
+using Project.Service.ViewModels.Invoice;
+
+namespace Project.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoansController : ControllerBase
+    {
+        private readonly ILoanService _loanService;
+
+        public LoansController(ILoanService loanService)
+        {
+            _loanService = loanService;
+        }
+
+        [HttpPost]
+        public IActionResult IssueLoan([FromBody] CreateInvoiceViewModel model)
+        {
+            var result = _loanService.IssueLoan(model);
+            return new ObjectResult(result);
+        }
+    }
+}
