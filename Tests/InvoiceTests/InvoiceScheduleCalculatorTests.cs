a5f11472dee328883e580ae32f8ccd67a9af6d4e
VahidDev <vrashidli1@gmail.com>
2023-01-30 00:46:40 +0400

    added CalculateInvoices_ShouldReturnSequentialInvoiceDatesStartingOneMonthAfterPayout test

--- Tests/InvoiceTests/InvoiceScheduleCalculatorTests.cs
+++ Tests/InvoiceTests/InvoiceScheduleCalculatorTests.cs
@@ -1,5 +1,7 @@
+using Microsoft.Data.SqlClient;
 using Moq;
 using Project.Infrastructure.Repositories.Abstraction;
+using Project.Infrastructure.SpModels;
 using Project.Service.Services.Implementation;
 using Project.Service.ViewModels;
 
@@ -8,7 +10,7 @@ namespace InvoiceTests
     public class InvoiceScheduleCalculatorTests
     {
         [Fact]
-        public void CalculateInvoices_ReturnsExpectedData_ForValidInput()
+        public void CalculateInvoices_ReturnsExpectedViewModel_ForValidInput()
         {
             //Arrange
             var invoiceRepositoryMock = new Mock<IInvoiceRepository>();
@@ -52,6 +54,41 @@ namespace InvoiceTests
                 Assert.True(invoice.Principal > 0);
 
                 validInvoiceDate = validInvoiceDate.AddMonths(1);
+            }
+        }
+
+        [Fact]
+        public void CalculateInvoices_ShouldReturnValidSequentialInvoiceDatesStartingOneMonthAfterPayout()
+        {
+            //Arrange
+            var loanPayoutDate = new DateTime(2022, 01, 01);
+            var model = new CalculateInvoicesViewModel
+            {
+                LoanAmount = 1000,
+                InterestRate = 10,
+                LoanPeriod = 12,
+                LoanPayOutDate = loanPayoutDate.ToString(),
+            };
+
+            var startPaymentDate = loanPayoutDate.AddMonths(1);
+            var loanDetailsSpParameters = new List<SqlParameter>
+                {
+                new SqlParameter(nameof(model.LoanAmount), model.LoanAmount),
+                new SqlParameter(nameof(model.InterestRate), model.InterestRate),
+                new SqlParameter(nameof(model.LoanPeriod), model.LoanPeriod),
+                new SqlParameter(nameof(startPaymentDate), startPaymentDate),
+                };
+
+            //Act
+            var newInvoices = EfDbTools.ExecuteProcedure<SP_CalcMonthlyPayments>
+                ("dbo.Sp_CalcMonthlyPayments", loanDetailsSpParameters);
+
+            //Assert
+            Assert.NotEmpty(newInvoices);
+
+            foreach (var invoice in newInvoices)
+            {
+                startPaymentDate = startPaymentDate.AddMonths(1);
             }
         }
     }
